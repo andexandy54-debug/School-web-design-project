dba05115cf02c49d6a536269813ac850ca8f1f94
Andy Andex <andexandy54@gmail.com>
2026-05-11 10:41:45 -0600

    Add files via upload

---
+++ register.php
@@ -0,0 +1,10 @@
+<form action="register.php" method="POST">
+        <h2>Register Account</h2>
+        <label>Username</label>
+        <input type="text" name="u_name" placeholder="Enter username" required>
+        
+        <label>Password</label>
+        <input type="password" name="u_pass" placeholder="Enter password" required>
+        
+        <button type="submit">Create Account</button>
+    </form>
